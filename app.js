177690907c6d0a6de27e7ea47e52ddd84fe4cf78
brittanya <m.brittanya@gmail.com>
2015-06-17 22:47:52 -0600

    Uploading Dan's example

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
-var app = angluar.module('app',[]);
+var app = angular.module('app', []);
 
-app.controller('myCtrl', ['$scope',function($scope){
-	$scope.message = "howdy";
-	$scope.filterBy = '';
-	$scope.setFilter(){
-		//set teh scope filter by what is clicked
+app.controller('myCtrl', ['$scope', function($scope){
+	$scope.filterBy = ''
+
+	$scope.setFilter = function(){
+		//set the $scope.filterBy according to what was clicked.
 	}
-	$scope.stuff = [
-	{ title: 'Sloppy Joe',
-		cost: 4.49,
-		notes: 'Extra Sloppy'}
-	];
 
+	$scope.transactions = [
+		{
+			title: 'Sloppy Joe',
+			cost: 4.49,
+			notes: 'Extra sloppy'
+		},
+		{
+			title: 'Crabby patty',
+			cost: 3.49,
+			notes: 'No pickles'
+		},
+		{
+			title: 'Hot dog',
+			cost: 2.49,
+			notes: 'Vegan'
+		},
+		{
+			title: 'Soda',
+			cost: 1.49,
+			notes: 'Large'
+		},
+		{
+			title: 'Skittles',
+			cost: 9.49,
+			notes: 'Taste the rainbow'
+		}
+	];
 }]);
